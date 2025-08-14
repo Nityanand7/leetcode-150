8948984059b8b769cf1f0c3e9ebeb8977e244785
Nityanand7 <msnityanand997@gmail.com>
2025-08-13 22:44:08 -0700

    adding a 3rd solution

--- leetcode-189.py
+++ leetcode-189.py
@@ -14,6 +14,7 @@ class Solution:
         nums[:] = res
 
 # solution with O(1) space complexity
+
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         n = len(nums)
@@ -30,3 +31,26 @@ class Solution:
         reverse(0, n - 1)   #all elements
         reverse(0, k - 1)   #first k elements
         reverse(k, n - 1)   #last n - k elemets
+
+# adding a 3rd solution using cycle replacements
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        if n == 0:
+            return
+        k %= n
+
+        count = 0
+        start = 0
+        while count < n:
+            curr = start
+            prev = nums[start]
+            while True:
+                nxt = (curr + k) % n
+                nums[nxt], prev = prev, nums[nxt]
+                curr = nxt
+                count += 1
+                if curr == start:
+                    break
+            start += 1
