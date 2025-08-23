74fb3e2651a49dd408f6cb0680d2f2347c2ba962
Nityanand7 <msnityanand997@gmail.com>
2025-08-23 13:28:07 -0700

    adding a space O(1) solution

--- leetcode-42.py
+++ leetcode-42.py
@@ -22,3 +22,22 @@ class Solution:
         
         return water
     
+# 42. Trapping Rain Water - Space Complexity O(1) - 2 pointers
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        
+        l, r = 0, len(height) - 1
+        leftMax, rightMax = height[l], height[r]
+        res = 0
+
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
+        return res
