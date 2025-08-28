87425a8adb53c75b0539fd7c0089553b9bb3282e
Nityanand7 <msnityanand997@gmail.com>
2025-08-28 10:52:24 -0700

    adding an alternate solution

--- leetcode-6.py
+++ leetcode-6.py
@@ -13,3 +13,19 @@ class Solution:
                 step = -1
             r += step
         return "".join(rows)
+
+# alternate solution - incremental counter 
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1: return s
+
+        res = ""
+        for r in range(numRows):
+            increment = 2 * (numRows - 1)
+            for i in range(r, len(s), increment):
+                res += s[i]
+                if (r > 0 and r < numRows - 1 and 
+                    i + increment - 2 * r < len(s)):
+                    res += s[i + increment - 2 * r]
+        return res
