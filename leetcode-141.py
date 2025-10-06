553e2a78e6a09407103891a66115cbbe26ab6146
Nityanand7 <msnityanand997@gmail.com>
2025-10-05 18:13:33 -0700

    Solution for lc 141

---
+++ leetcode-141.py
@@ -0,0 +1,25 @@
+# using a hashset
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+    
+# two pointer solution - Floyd's tortoise and hare algorithm
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
