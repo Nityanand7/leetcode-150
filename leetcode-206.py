341c78bef9b62125c9bf46f36ca9914284f71534
Nityanand7 <msnityanand997@gmail.com>
2025-10-09 10:49:26 -0700

    Solution for lc 206

---
+++ leetcode-206.py
@@ -0,0 +1,27 @@
+# Iterative solution 
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+
+        while cur:
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+        return prev
+    
+# Recursive solution
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+
+        newHead = head
+        if head.next:
+            newHead = self.reverseList(head.next)
+            head.next.next = head
+        head.next = None
+
+        return newHead
