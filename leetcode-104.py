67a86a76a372969c8e582502e131e25177072129
Nityanand7 <msnityanand997@gmail.com>
2025-10-20 07:37:08 -0700

    Solution for lc 104

---
+++ leetcode-104.py
@@ -0,0 +1,43 @@
+# dfs recrsive solution
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    
+# bfs iterative solution
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        level = 0
+        q = deque([root])
+        while q:
+
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                
+            level += 1
+
+        return level
+
+# dfs iterative solution
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        stack = [[root, 1]]
+        res = 0
+
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                res = max(res, depth)
+                stack.append([node.right, depth + 1])
+                stack.append([node.left, depth + 1])
+        return res
